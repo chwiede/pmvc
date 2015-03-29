2be3a3e10c169a45cbcd42776f648bff2cb83940
Christoph Wiedemann <chwiede@gmail.com>
2015-03-29 12:34:58 +0200

    bugfix: filepaths for locale and icons

--- src/pmvc.py
+++ src/pmvc.py
@@ -8,13 +8,13 @@ from modules.localization import localize
 
 if __name__ == '__main__':
     # setup localization
-    localize(os.path.abspath('./locale'))
+    localize(os.path.dirname(__file__) + '/locale')
 
     # load app
     app = QApplication(sys.argv)
 
     # start app
-    mainWindow = MainWindow(os.path.abspath('icons'))
+    mainWindow = MainWindow(os.path.dirname(__file__) + '/icons')
     mainWindow.show()
 
     # bye bye
